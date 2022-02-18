635e7148bc9844c4c032dc74bbd340c2739685e5
Serhii <brykseryi@gmail.com>
2022-02-18 19:51:42 +0200

    Commit #4, js added (learning js)

--- Main.js
+++ Main.js
@@ -20,7 +20,7 @@ personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
 */
-//second lesson
+//second lesson  terms
 /*if (4 == 9) {
   console.log("Ok!");
 } else {
@@ -57,7 +57,7 @@ switch (num) {
     break;
 } */
 
-//lesson 3
+//lesson 3 cycles(3 types)
 
 // let num = 50;
 
@@ -82,7 +82,7 @@ switch (num) {
 //   console.log(i);
 // }
 
-//lesson 4
+//lesson 4  practice  (if, if-else, for)
 /*
 let numberOfFilms = +prompt("How many films have you watched?", "");
 
@@ -120,8 +120,8 @@ if (personalMovieDB.count < 10) {
 console.log(personalMovieDB);
 */
 
-//lesson 5,6,7
-
+//lesson 5,6,7  functions
+/*
 let numberOfFilms;
 function start() {
   numberOfFilms = +prompt("How many films have you watched?", "");
@@ -190,3 +190,86 @@ function writeYourGenres() {
 writeYourGenres();
 
 // console.log(personalMovieDB);
+*/
+
+//lesson 8  callback in functions
+
+// function learnJS(lang, callback) {
+//   console.log(`I learn: ${lang}`);
+//   callback();
+// }
+
+// learnJS("JavaScript", function () {
+//   console.log("I passed this lesson!");
+// });
+
+//lesson 9 objects and destructurization
+
+/* const options = {
+  name: "test",
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: "black",
+    bg: "red",
+  },
+  makeTest: function () {
+    console.log("Test");
+  },
+};
+
+options.makeTest();
+
+const { border, bg } = options.colors;
+console.log(border);          */
+
+// console.log(Object.keys(options).length);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//   if (typeof options[key] === "object") {
+//     for (let i in options[key]) {
+//       console.log(`Option ${i} has value ${options[key][i]}`);
+//       counter++;
+//     }
+//   } else {
+//     console.log(`Option ${key} has value ${options[key]}`);
+//   }
+// }
+// console.log(counter);
+
+// lesson 10 arrays and pseudo-arrays
+
+// const arr = [2, 28, 26, 8, 10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//   return a - b;
+// }
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function (item, i, arr) {
+//   console.log(`${i}: ${item} inside the array ${arr}`);
+// });
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//   console.log(arr);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// console.log(products.join("; "));
